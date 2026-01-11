b9440083ef1504b84cc7b21037f94fd62a708074
hitoriii <Prarthanagade25@gmail.com>
2026-01-12 00:04:06 +0530

    added authentifications

---
+++ src/utils/cookies.js
@@ -0,0 +1,15 @@
+export const cookies = {
+  getOptions: () => ({ sameSite: 'strict' }),
+
+  set: (res, name, value, options = {}) => {
+    res.cookie(name, value, { ...cookies.getOptions(), ...options });
+  },
+
+  clear: (res, name, options = {}) => {
+    res.clearCookie(name, { ...cookies.getOptions(), ...options });
+  },
+
+  get: (req, name) => {
+    return req.cookies && req.cookies[name];
+  },
+};
