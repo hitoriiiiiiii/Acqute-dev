b207194828e8d47637cd8e340268c48b91488ff1
hitoriii <Prarthanagade25@gmail.com>
2026-01-11 19:11:47 +0530

    added drizzle neon database

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (res,req) => {
-    res.status(200).send('Hello from acquistions !')
-})
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from acquistions !');
+});
 
 export default app;
